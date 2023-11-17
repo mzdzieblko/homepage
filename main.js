5ad691c4276f9294be44438af3ef6485489f9f20
mzdzieblko <51722512+mzdzieblko@users.noreply.github.com>
2023-11-17 10:04:40 +0100

    learning js

---
+++ main.js
@@ -0,0 +1,7 @@
+console.log('testing, testing, jak mnie słychac?');
+
+const firstName = 'Mateuszek';
+const age = 34;
+
+console.log(`Nazywam się ${firstName} i mam  ${age} lat😎`);
+console.log(age);
